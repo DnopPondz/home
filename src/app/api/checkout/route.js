fc1b13144de1002f8bbe08cac1d1ac4f48e2d18f
pete <pete@movaci.com>
2025-08-05 16:06:53 +0700

    fix: delete console log API

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-console.log("🔑 STRIPE_SECRET_KEY:", process.env.STRIPE_SECRET_KEY);
+
 
 export async function POST(req) {
   const body = await req.json();
