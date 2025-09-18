a4e961d1f783d0d5d894cf4a40bf18607fe06158
pete <pete@movaci.com>
2025-09-18 14:34:41 +0700

    add nitification pop up and clear

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -415,7 +415,7 @@ const Navbar = () => {
 
       {showAllNotifications && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          className="fixed inset-0 z-50 backdrop-blur-sm flex items-center justify-center  bg-opacity-50"
           onClick={closeAllNotifications}
         >
           <div
